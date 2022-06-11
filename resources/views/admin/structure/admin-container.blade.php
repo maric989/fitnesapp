bd3e5fb5b9d87bd6d3c9cdee6a67bd396950e979
Nikola Milenkovic <nikola.milenkovic@quantox.com>
2022-06-11 23:47:13 +0200

    - Make admin menu - Add fonts

--- resources/views/admin/structure/admin-container.blade.php
+++ resources/views/admin/structure/admin-container.blade.php
@@ -1,10 +1,13 @@
 @extends('structure.container')
+@section('head-css')
+    <link rel="stylesheet" href="{{ mix('/css/admin/general.css') }}" />
+@endsection
 @section('main-container')
-    <div>
-        <div>
+    <div class="admin-container">
+        <div class="admin-left-menu fixed">
             @include('admin.structure.left-menu')
         </div>
-        <div>
+        <div class="admin-body">
             @yield('admin-container')
         </div>
     </div>
