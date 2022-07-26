a5288f1107467591aab287057efd12692625a075
Nikola Milenkovic <dzoni82.kg@gmail.com>
2022-07-26 23:30:52 +0200

    Show lessons on program view

--- resources/views/admin/program/view.blade.php
+++ resources/views/admin/program/view.blade.php
@@ -43,33 +43,90 @@
             <div class="title semi-bold">Lessons</div>
             <div class="lessons-list">
                 <div class="flex space-between">
-                    @foreach($program->lessons as $lesson)
-                    <a class="first-lesson relative" href="{{ route('admin.lesson.edit', $lesson->id) }}">
-                        <img src="{{ $lesson->coverImage->full_path }}" class="block full lesson-list-image" />
+                    <a class="first-lesson relative" href="{{ route('admin.lesson.edit', $program->lessons[0]->id) }}">
+                        <img src="{{ $program->lessons[0]->coverImage->full_path }}" class="block full lesson-list-image" />
                         <div class="images-lesson-overlay absolute flex v-align-center h-align-center">
                             <div class="play-normal"></div>
                         </div>
                         <div class="lesson-data-top absolute">
                             <div class="flex">
                                 <div class="additional-info-item font-medium-weight flex v-align-center pointer">
-                                    {{ $lesson->intensity->name }}
+                                    {{ $program->lessons[0]->intensity->name }}
                                 </div>
                                 <div class="additional-info-item font-medium-weight flex v-align-center pointer">
-                                    {{ $lesson->difficulty->name }}
+                                    {{ $program->lessons[0]->difficulty->name }}
                                 </div>
                             </div>
                         </div>
                         <div class="lesson-data-bottom absolute">
-                            <div class="lesson-day">Day {{ $lesson->pivot->day }}</div>
-                            <div class="lesson-title font-medium-weight">{{ $lesson->title }}</div>
-                            <div class="lesson-description">{{ $lesson->short_description }}</div>
+                            <div class="lesson-day">Day {{ $program->lessons[0]->pivot->day }}</div>
+                            <div class="lesson-title font-medium-weight">{{ $program->lessons[0]->title }}</div>
+                            <div class="lesson-description">{{ $program->lessons[0]->short_description }}</div>
                         </div>
                     </a>
-                    @endforeach
-{{--                    <div class="second-lessons">--}}
-
-                    </div>
+                    @if ($program->lessons->count() > 1)
+                        <div class="second-lessons">
+                            <div class="second-lesson">
+                                <a href="{{ route('admin.lesson.edit', $program->lessons[1]->id) }}" class="relative block">
+                                    <img src="{{ $program->lessons[1]->coverImage->full_path }}" class="block full lesson-list-image" />
+                                    <div class="images-lesson-overlay absolute flex v-align-center h-align-center">
+                                        <div class="play-normal second"></div>
+                                    </div>
+                                    <div class="lesson-data-top second absolute">
+                                        <div class="lesson-day">Day {{ $program->lessons[2]->pivot->day }}</div>
+                                    </div>
+                                    <div class="lesson-data-bottom second absolute">
+                                        <div class="lesson-title font-medium-weight">{{ $program->lessons[1]->title }}</div>
+                                    </div>
+                                </a>
+                            </div>
+                            @if ($program->lessons->count() > 2)
+                                <div class="second-lesson">
+                                    <a href="{{ route('admin.lesson.edit', $program->lessons[2]->id) }}" class="relative block">
+                                        <img src="{{ $program->lessons[2]->coverImage->full_path }}" class="block full lesson-list-image" />
+                                        <div class="images-lesson-overlay absolute flex v-align-center h-align-center">
+                                            <div class="play-normal second"></div>
+                                        </div>
+                                        <div class="lesson-data-top second absolute">
+                                            <div class="lesson-day">Day {{ $program->lessons[2]->pivot->day }}</div>
+                                        </div>
+                                        <div class="lesson-data-bottom second absolute">
+                                            <div class="lesson-title font-medium-weight">{{ $program->lessons[2]->title }}</div>
+                                        </div>
+                                    </a>
+                                </div>
+                            @endif
+                        </div>
+                    @endif
                 </div>
+                @if ($program->lessons->count() > 3)
+                    <div class="flex flex-wrap space-between more-lessons">
+                        @foreach($program->lessons as $key => $lesson)
+                            @if($key > 2)
+                                <a class="other-lesson relative" href="{{ route('admin.lesson.edit', $lesson->id) }}">
+                                    <img src="{{ $lesson->coverImage->full_path }}" class="block full lesson-list-image" />
+                                    <div class="images-lesson-overlay absolute flex v-align-center h-align-center">
+                                        <div class="play-normal other"></div>
+                                    </div>
+                                    <div class="lesson-data-top absolute">
+                                        <div class="flex">
+                                            <div class="additional-info-item font-medium-weight flex v-align-center pointer">
+                                                {{ $lesson->intensity->name }}
+                                            </div>
+                                            <div class="additional-info-item font-medium-weight flex v-align-center pointer">
+                                                {{ $lesson->difficulty->name }}
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="lesson-data-bottom absolute">
+                                        <div class="lesson-day">Day {{ $lesson->pivot->day }}</div>
+                                        <div class="lesson-title font-medium-weight">{{ $lesson->title }}</div>
+                                    </div>
+                                </a>
+                            @endif
+                        @endforeach
+                    </div>
+                @endif
             </div>
         </div>
     @endif
