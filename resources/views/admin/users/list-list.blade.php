6062389eb725697405e4d2b1be59f8f3bb40e416
Nikola Milenkovic <dzoni82.kg@gmail.com>
2022-08-10 16:33:02 +0200

    Fix user list

--- resources/views/admin/users/list-list.blade.php
+++ resources/views/admin/users/list-list.blade.php
@@ -1,24 +1,6 @@
 @section('head-css')
     @parent
     <link rel="stylesheet" href="{{ mix('/css/admin/program-list-list.css') }}" />
+    <link rel="stylesheet" href="{{ mix('/css/admin/user-list-list.css') }}" />
 @endsection
-<div class="table full data-grid">
-    <div class="table-row head bold">
-        <div class="table-cell semi-bold">Name</div>
-        <div class="table-cell semi-bold">Email</div>
-        <div class="table-cell semi-bold">Country</div>
-        <div class="table-cell semi-bold">Gender</div>
-        <div class="table-cell semi-bold">Status</div>
-        <div class="table-cell semi-bold">Action</div>
-    </div>
-    @foreach($users as $user)
-        <a class="table-row body program-list-link" href="{{ route('admin.user.edit', ['user_id' => $user->id]) }}">
-            <div class="table-cell v-align-middle">{{ $user->first_name }} {{ $user->last_name }}</div>
-            <div class="table-cell v-align-middle">{{ $user->email }}</div>
-            <div class="table-cell v-align-middle">{{ $user->country->name }}</div>
-            <div class="table-cell v-align-middle">{{ $user->gender }}</div>
-            <div class="table-cell v-align-middle">{{ $user->roles[0]->name }}</div>
-            <div class="table-cell v-align-middle"></div>
-        </a>
-    @endforeach
-</div>
+
--- resources/views/admin/users/paginate.blade.php
+++ resources/views/admin/users/paginate.blade.php
@@ -1,12 +1,36 @@
 @extends('admin.structure.admin-container')
+@section('head-css')
+    @parent
+    <link rel="stylesheet" href="{{ mix('/css/admin/program-list-list.css') }}" />
+    <link rel="stylesheet" href="{{ mix('/css/admin/user-list-list.css') }}" />
+@endsection
 @section('admin-container')
-    @include('admin/common/search-bar', ['placeholder' => 'Search Programs..'])
-    @include('admin/common/title-bar', ['title' => 'Users', 'view' => 'list', 'view_route' => 'admin.users.paginate'])
+    @include('admin/common/search-bar', ['placeholder' => 'Search Users..'])
+    @include('admin/common/title-bar', ['title' => 'Users'])
 
     @if($users)
-        <div class="flex flex-wrap space-between program-list-container">
-            @include('admin/users/list-list', ['users' => $users])
+        <div class="user-list-container">
+            <div class="flex flex-wrap space-between program-list-container">
+                <div class="table full data-grid">
+                    <div class="table-row head bold">
+                        <div class="table-cell semi-bold">Name</div>
+                        <div class="table-cell semi-bold">Email</div>
+                        <div class="table-cell semi-bold">Country</div>
+                        <div class="table-cell semi-bold">Gender</div>
+                        <div class="table-cell semi-bold">Status</div>
+                    </div>
+                    @foreach($users as $user)
+                        <a class="table-row body program-list-link" href="{{ route('admin.user.edit', ['user_id' => $user->id]) }}">
+                            <div class="table-cell v-align-middle">{{ $user->first_name }} {{ $user->last_name }}</div>
+                            <div class="table-cell v-align-middle">{{ $user->email }}</div>
+                            <div class="table-cell v-align-middle">{{ $user->country->name }}</div>
+                            <div class="table-cell v-align-middle">{{ $user->gender }}</div>
+                            <div class="table-cell v-align-middle">{{ $user->roles[0]->name }}</div>
+                        </a>
+                    @endforeach
+                </div>
+            </div>
+            {{ $users->links('admin/structure/paginate') }}
         </div>
-        {{ $users->links('admin/structure/paginate') }}
     @endif
 @endsection
