defdb8c0f7f27449007f3e9442029f0ca9300de1
maric <maric989@gmail.com>
2022-08-16 20:11:30 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/', function () {
+    return '<p>Hi!
+                In case you want to continue working on the project
+                feel free to reach me out on maric989@gmail.com
+            </p>';
+});
+
+
 require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
